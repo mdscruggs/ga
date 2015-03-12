e9fd4ddd171ff17a7ae6b7d7faf63432a36d2646
mscruggs <mdscruggs@gmail.com>
2015-03-11 21:21:34 -0400

    Add setup.py, ignore build dir

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ga',
+      version='dev',
+      license='GNU GPLv2',
+      description='A lightweight genetic algorithm library written in pure Python',
+      author='Matt Scruggs',
+      url='https://github.com/mdscruggs/ga',
+      packages=['ga', 'ga.examples'],
+     )
