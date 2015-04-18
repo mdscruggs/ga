d9bd4fa067681e5af150560f51d8c492b9c9f045
mscruggs <mdscruggs@gmail.com>
2015-04-18 17:06:28 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='ga',
-      version='dev',
-      license='GNU GPLv2',
+      version='0.1.1.dev',
+      license='GNU GPLv3',
       description='A lightweight genetic algorithm library written in pure Python',
       author='Matt Scruggs',
       url='https://github.com/mdscruggs/ga',
